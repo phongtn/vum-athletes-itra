a41af5b2c345f2f128867d26399d8dbadfc33881
PhongTn <phongtn.group1@gmail.com>
2025-03-06 11:52:31 +0700

    update the title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'Vietnam Ultra Marathon Athletes - ITRA',
+  description: 'a web application that displays information about trail running athletes, specifically those who are part of the International Trail Running Association (ITRA)',
   generator: 'v0.dev',
 }
 
